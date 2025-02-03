7ab659f2c73119f7c9b78423dc11f27776d3f05d
정규재 <wjdrbwo1206@naver.com>
2025-02-03 13:29:21 +0900

    prettier setting

--- app/test.test.ts
+++ app/test.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from 'vitest';
 
-describe("Test", () => {
-    it("1 + 1 = 2", () => {
-        expect(1 + 1).toBe(2);
-    })
-})
+describe('Test', () => {
+  it('1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tailwindcss(), !process.env.VITEST && reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    !process.env.VITEST && reactRouter(),
+    tsconfigPaths(),
+  ],
   build: {
     cssMinify: true,
     ssr: false,
   },
-  test:{
+  test: {
     environment: 'jsdom',
     globals: true,
-  }
+  },
 });
