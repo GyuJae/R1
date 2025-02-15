839dc578cd9b228b491fed4fbc0a1c998f32a2c8
GyuJae <wjdrbwo1206@naver.com>
2025-02-15 11:13:18 +0900

    eslint plugin unicorn 추가가

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,4 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const db = drizzle(process.env.DATABASE_URL!);
-
-export default db;
+const database = drizzle(process.env.DATABASE_URL!);
+export default database;
--- eslint/react.config.cjs
+++ eslint/react.config.cjs
@@ -1,5 +1,6 @@
 const reactPlugin = require('eslint-plugin-react');
 const hooksPlugin = require('eslint-plugin-react-hooks');
+const unicornPlugin = require('eslint-plugin-unicorn');
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 module.exports = [
@@ -8,10 +9,12 @@ module.exports = [
     plugins: {
       react: reactPlugin,
       'react-hooks': hooksPlugin,
+      unicorn: unicornPlugin,
     },
     rules: {
       ...reactPlugin.configs['jsx-runtime'].rules,
       ...hooksPlugin.configs.recommended.rules,
+      ...unicornPlugin.configs.recommended.rules,
       'react/prop-types': 'off',
       'react-hooks/exhaustive-deps': 'off',
       'react/display-name': 'off',
