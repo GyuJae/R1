988c70838de42d31f033369178399b0aafc00c87
GyuJae <wjdrbwo1206@naver.com>
2025-02-15 15:14:03 +0900

    앨범 페이지 추가가

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,11 @@ import type { RouteConfig } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
+
   layout('routes/auth/layout.tsx', [route('auth', 'routes/auth/page.tsx')]),
+
+  layout('routes/albums/layout.tsx', [
+    route('albums', 'routes/albums/page.tsx'),
+    route('albums/:id', 'routes/albums/[id]/page.tsx'),
+  ]),
 ] satisfies RouteConfig;
---
+++ app/routes/albums/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function AlbumDetailPage() {
+  return <main>AlbumDetail</main>;
+}
