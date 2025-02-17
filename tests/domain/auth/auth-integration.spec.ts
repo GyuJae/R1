c2dfe597329b3a3e25b6ffb32567920ec16f1588
정규재 <wjdrbwo1206@naver.com>
2025-02-17 16:31:25 +0900

    테스트 환경 설정 진행 중

--- tests/domain/auth/auth-integration.spec.ts
+++ tests/domain/auth/auth-integration.spec.ts
@@ -1,15 +1,21 @@
 import { container } from 'tsyringe';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { beforeAll, beforeEach, describe, expect, it } from 'vitest';
 import { CreateUserRequest } from '~/domains/auth/dtos/create-user-request';
 import { DrizzleUserRepository } from '~/domains/auth/repositories/drizzle-user.repository';
 import type { UserRepository } from '~/domains/auth/repositories/user.repository';
+import database from 'db';
+import { afterEach } from 'node:test';
 
 describe('계정 관련 테스트', () => {
   let userRepository: UserRepository;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     userRepository = container.resolve(DrizzleUserRepository);
   });
+
+  beforeEach(async () => {});
+
+  afterEach(async () => {});
 
   it('계정을 이메일을 통해 생성할 수 있습니다.', async () => {
     // Given
